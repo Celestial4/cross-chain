9538a018a44c6d9ed631f852213ce9e3326ddaa7
clm_lab <clm4571@gmail.com>
2023-04-14 18:16:08 +0800

    优化通用性部署脚本

--- platform/script/start-gateway.sh
+++ platform/script/start-gateway.sh
@@ -1,2 +1,17 @@
 #! /bin/bash
-java -cp 'app/*:lib/*:conf/*' com.crosschain.CrossChainGatewayApplication &>gateway-fabric.log &
+cur_dir=$(pwd)
+
+blockchain_name=$(pwd|awk -F '/' '{print $NF}'|awk -F '-' '{print $1}')
+
+echo "starting gateway-$blockchain_name...."
+
+java -Dspring.config.location=$cur_dir/conf/ -cp 'app/*:lib/*:conf/*' com.crosschain.CrossChainGatewayApplication &>gateway-$blockchain_name.log &
+sleep 5
+
+success_info=$(tail -n 1 gateway-${blockchain_name}.log |grep Started)
+
+if [ -n "$success_info" ]; then
+  echo "start gateway-${blockchain_name} successfully!"
+else
+  echo "something goes wrong when start gateway, please check gateway-$blockchain_name.log"
+fi
--- platform/script/stop-gateway.sh
+++ platform/script/stop-gateway.sh
@@ -1,15 +1,37 @@
 #! /bin/bash
-pid=$(lsof -i :3001 | awk 'NR==2{print $2}')
+declare -A map
+map=(
+  [fabric]=3001
+  [ca]=3002
+  [bcos]=3003
+  [hyperchain]=3004
+  [ethereum]=3005
+)
+
+blockchain_name=$(pwd | awk -F '/' '{print $NF}' | awk -F '-' '{print $1}')
+
+for i in "${!map[@]}"; do
+        if [[ $i =~ ${blockchain_name} ]]; then
+            index=$i
+        fi
+done
+
+if [ -z "${index}" ]; then
+    echo "check your deployment directory, make sure that name of the deployment directory compatible with chain name"
+    exit 1
+fi
+
+pid=$(lsof -i :"${map[$index]}" | awk 'NR==2{print $2}')
 if [ -n "$pid" ]; then
-	echo "fabric gateway is running. pid:$pid"
+  echo "fabric gateway is running. pid:$pid"
 else
-	echo "fabric gateway is not running"
-	exit 0
+  echo "fabric gateway is not running"
+  exit 0
 fi
 
 kill -9 $pid
-if [ $! == 0 ]; then
-	echo "kill fabric gateway successfully"
+if [ $? == 0 ]; then
+  echo "kill fabric gateway successfully"
 else
-	echo "something goes wrong"
+  echo "something goes wrong"
 fi
